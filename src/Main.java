5b034767a55a24aca7393e4a59f3c37330f8f9fe
AndreySN7 <andrey__n@mail.ru>
2026-05-14 22:30:59 +0300

    Решение задачи "Разворот строки с сохранением символов на месте"
    Попытка номер два

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,38 @@ public class Main {
 		String input = "J@va the be$t!123";
 
 		System.out.println(reversalStringWithoutCharacters(input));
+		System.out.println(reversalStringWithoutCharactersBest(input));
+	}
+
+	private static String reversalStringWithoutCharactersBest(String input) {
+		if (input == null || input.isEmpty()) {
+			return "";
+		}
+
+		int start = 0;
+		int end = input.length() - 1;
+		char[] result = input.toCharArray();
+		while (start <= end) {
+			if (!Character.isLetter(input.charAt(start))) {
+				start++;
+				continue;
+			}
+			if (!Character.isLetter(input.charAt(end))) {
+				end--;
+				continue;
+			}
+			char temp = result[start];
+			result[start++] = result[end];
+			result[end--] = temp;
+		}
+		return new String(result);
 	}
 
 	private static String reversalStringWithoutCharacters(String input) {
+		if (input == null || input.isEmpty()) {
+			return "";
+		}
+
 		StringBuilder result = new StringBuilder();
 		for (char c : input.toCharArray()) {
 			if (Character.isLetter(c)) {
