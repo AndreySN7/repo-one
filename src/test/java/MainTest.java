5f5c79d73563cf7e3f6277de905a29f08dade60f
AndreySN7 <andrey__n@mail.ru>
2026-05-19 00:38:37 +0300

    Тестирование первой задачи
    Первые тесты

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,13 +1,37 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("Тестирование Main")
 public class MainTest {
+
+	@DisplayName("Тестирование на возврат пустой строки, если строка пустая")
 	@Test
 	public void emptyStringShouldReturnEmpty() {
-		String input = "";
+		String inputEmpty = "";
+
+		String result = Main.reverseLetters(inputEmpty);
+
+		Assertions.assertEquals("", result, "Должен вернуть пустую строку");
+	}
+
+	@DisplayName("Тестирование на возврат пустой строки, если строка null")
+	@Test
+	public void nullStringShouldReturnEmpty() {
+		String inputNull = null;
+
+		String resultNull = Main.reverseLetters(inputNull);
+
+		Assertions.assertEquals("", resultNull, "Должен вернуть пустую строку");
+	}
+
+	@DisplayName("Проверяем, развернулась ли строка")
+	@Test
+	public void stringShouldReturn() {
+		String input = "te$t $tr1hg 123Q!";
 
 		String result = Main.reverseLetters(input);
 
-		Assertions.assertEquals("", result);
+		Assertions.assertEquals("Qg$h $rt1te 123t!", result);
 	}
 }
